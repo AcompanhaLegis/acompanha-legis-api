66a199e8f6e1f899a578bd09bf9ad4d015699619
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2020-06-03 22:34:11 +0200

    Add api users serializers and auth

--- api/api/settings.py
+++ api/api/settings.py
@@ -37,6 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
 ]
 
 MIDDLEWARE = [
--- api/api/urls.py
+++ api/api/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework.authtoken import views
+
+from user import views as user_views
+
+router = routers.DefaultRouter()
+router.register(r'users', user_views.UserViewSet)
+router.register(r'groups', user_views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', views.obtain_auth_token),
+    path('', include(router.urls)),
 ]
