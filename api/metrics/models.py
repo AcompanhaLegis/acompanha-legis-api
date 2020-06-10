13bfd0a3e2cd9ef94f63f70fa194f88273945bdf
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2020-06-10 19:55:26 +0200

    Add deputado twitter field and change primary key

--- api/metrics/models.py
+++ api/metrics/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.contrib import admin
 from django.contrib.postgres.fields import JSONField
 
 
 class DeputadoMetrics(models.Model):
-    external_id = models.IntegerField(null=False, blank=False, unique=True)
+    external_id = models.IntegerField(primary_key=True, null=False, blank=False, unique=True, editable=True)
+    twitter = models.CharField(max_length=255, null=True, blank=True)
     metrics = JSONField()
 
     def __str__(self):
         return 'DeputadoMetrics(id:{})'.format(self.external_id)
+
+
+admin.site.register(DeputadoMetrics)
