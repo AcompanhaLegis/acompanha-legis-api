0990ce8e5edca8882ebe0a42b7b471f9578f826a
RafaellaJunqueira <rafaellafjunqueira@gmail.com>
2020-10-10 18:23:55 -0300

    Add new user serializer test

--- api/user/tests.py
+++ api/user/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from user.serializers import NewUserSerializer
+from user.models import CustomUser
 
-# Create your tests here.
+class NewUserSerializerTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_create(self):
+        fake_user = {
+            "email": "blabla@gmail.com",
+            "password": "123deoliveira4",
+        }
+        new_user_serializer = NewUserSerializer()
+        user = new_user_serializer.create(fake_user)
+
+        self.assertEqual(user.email, "blabla@gmail.com")
+        self.assertTrue(user.check_password("123deoliveira4"))
+           
